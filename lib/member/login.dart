a5dc811d92446d02ae291c886bb88b844698a990
betbies <betniss11@gmail.com>
2024-03-10 13:30:25 +0300

    Commit mesajınız

--- lib/member/login.dart
+++ lib/member/login.dart
@@ -4,13 +4,14 @@ import 'package:confetti/confetti.dart';
 import '../main.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _scaleAnimation;
   late ConfettiController _confettiController;
@@ -35,132 +36,174 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
+      resizeToAvoidBottomInset: false,
       body: Stack(
-        children: [
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'E-mail',
-                    prefixIcon: const Icon(
-                      Icons.email,
-                      color: Color(0xFF2980B9),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide:
-                      const BorderSide(color: Color(0xFF2980B9), width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide:
-                      const BorderSide(color: Color(0xFF2980B9), width: 1.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                      color: Color(0xFF2980B9),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide:
-                      const BorderSide(color: Color(0xFF2980B9), width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide:
-                      const BorderSide(color: Color(0xFF2980B9), width: 1.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    _animationController.forward();
-                    _confettiController.play();
-                    // Burada giriş işlemleri yapılabilir
-                    Future.delayed(const Duration(milliseconds: 300), () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MyHomePage()),
-                      );
-                      _animationController.reverse();
-                    });
-                  },
-                  child: AnimatedBuilder(
-                    animation: _animationController,
-                    builder: (context, child) {
-                      return Transform.scale(
-                        scale: _scaleAnimation.value,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: const Color(0xFF2980B9).withOpacity(0.5),
-                                blurRadius: 15,
-                              ),
-                            ],
+        children: <Widget>[
+          Positioned.fill(
+            child: Container(
+              color: Color(0xFFE0E0E0),
+            ),
+          ),
+          Image.asset(
+            'assets/images/LAWW.jpeg',
+            height: MediaQuery.of(context).size.height * 0.55,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.fitHeight,
+          ),
+          Positioned.fill(
+            child: Container(
+              color: Colors.white.withOpacity(0.5),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 0.25),
+            child: Transform.rotate(
+              angle: pi / 4,
+              child: Material(
+                elevation: 5,
+                shadowColor: Colors.black,
+                color: Color(0xFF2980B9),
+                borderRadius: BorderRadius.circular(32),
+                child: Container(
+                  height: MediaQuery.of(context).size.width / 2 * sqrt(2),
+                  width: MediaQuery.of(context).size.width / 2 * sqrt(2),
+                  child: Transform.rotate(
+                    angle: -pi / 4,
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'LAW AI',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
                           ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Container(
-                              color: const Color(0xFF2980B9),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 16,
-                                horizontal: 32,
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(
+                              color: Color(0xFF0B1754),
+                            ),
+                            decoration: InputDecoration(
+                              filled: false,
+                              contentPadding: EdgeInsets.all(10),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Color(0xFF0B1754), width: 2.0),
                               ),
-                              child: const Text(
-                                'Login',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Color(0xFF000000), width: 1.0),
+                              ),
+                              hintText: 'Your Email',
+                              hintStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    },
+                          SizedBox(height: 16),
+                          TextFormField(
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: true,
+                            style: TextStyle(
+                              color: Color(0xFF0B1754),
+                            ),
+                            decoration: InputDecoration(
+                              filled: false,
+                              contentPadding: EdgeInsets.all(10),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Color(0xFF0B1754), width: 2.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Color(0xFF000000), width: 1.0),
+                              ),
+                              hintText: 'Your Password',
+                              hintStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 12),
+                          GestureDetector(
+                            onTap: () {
+                              _animationController.forward();
+                              _confettiController.play();
+                              Future.delayed(const Duration(milliseconds: 300), () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const MyHomePage(),
+                                  ),
+                                );
+                                _animationController.reverse();
+                              });
+                            },
+                            child: AnimatedBuilder(
+                              animation: _animationController,
+                              builder: (context, child) {
+                                return Transform.scale(
+                                  scale: _scaleAnimation.value,
+                                  child: ClipOval(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Color(0xFF0B1754).withOpacity(0.5),
+                                            blurRadius: 15,
+                                          ),
+                                        ],
+                                      ),
+                                      child: Container(
+                                        color: Color(0xFFE0E0E0),
+                                        padding: const EdgeInsets.all(16),
+                                        child: Icon(
+                                          Icons.balance_rounded,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            alignment: Alignment.topCenter,
-            child: const Column(
-              children: [
-                Text(
-                  'LAW AI',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF000000),
-                  ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 12),
+              child: Text(
+                'Go To Sign Up',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                Icon(
-                  Icons.balance_rounded, // Hukuk simgesi
-                  size: 44,
-                  color: Color(0xFF000000),
-                ),
-              ],
+              ),
             ),
           ),
           Align(
